0bf2abad89e95c72ec0487228bfd484b67e53c4d
Bryan Bess <squarejaw@bsbess.com>
2015-12-30 16:32:21 -0600

    Setup Travis

--- test/helpers/index.js
+++ test/helpers/index.js
@@ -1,5 +1,5 @@
 var massive = require("../../index");
-var connectionString = "postgres://rob:password@localhost/massive";
+var connectionString = "postgres://postgres@localhost/massive";
 var assert = require("assert");
 var path = require("path");
 var scriptsDir = path.join(__dirname, "..", "db");
@@ -8,7 +8,7 @@ exports.connectionString = connectionString;
 
 exports.init = function(next){
   massive.connect({
-    connectionString : connectionString, 
+    connectionString : connectionString,
     scripts : scriptsDir}, next);
 };
 
@@ -22,25 +22,7 @@ exports.resetDb = function(next){
   });
 };
 
-var doc1 = {
-  title : "A Document",
-  price : 22.00,
-  description : "lorem ipsum etc",
-  is_good : true,
-  created_at : new Date()
-};
-var doc2 = {
-  title : "Another Document",
-  price : 18.00,
-  description : "Macaroni and Cheese",
-  is_good : true,
-  created_at : new Date()
-};
-var doc3 = {
-  title : "Starsky and Hutch",
-  price : 6.00,
-  description : "Two buddies fighting crime",
-  is_good : false,
-  created_at : new Date()
-};
+before(function(done){
+  exports.resetDb(done);
+});
 
