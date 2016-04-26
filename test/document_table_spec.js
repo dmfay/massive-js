397afef9fd6e15d412f8f368e6eb6d41fbe7af12
Henrik Larsen Toft <henrik@vimond.com>
2016-04-26 15:07:49 +0200

    Added create/drop schema and tables specs

---
+++ test/document_table_spec.js
@@ -0,0 +1,110 @@
+var assert = require("assert");
+var _ = require('underscore')._;
+var helpers = require("./helpers");
+var db;
+var schema = 'spec';
+var tableName = 'doggies';
+var schemaTableName = schema + '.' + tableName;
+
+describe('Document table', function () {
+
+  beforeEach(function(done){
+    helpers.resetDb(function(err,res){
+      db = res;
+      done();
+    });
+  });
+
+  afterEach(function(done) {
+    db.dropSchema(schema, {cascade: true}, function(err) {
+      assert.ifError(err);
+      db.dropDocumentTable(tableName, {cascade: true}, function(err) {
+        assert.ifError(err);
+        done();
+      });
+    });
+  });
+
+  describe('create', function() {
+
+    describe('without schema', function() {
+
+      it('creates a table on public schema', function(done) {
+        db.createDocumentTable(tableName, function(err, res) {
+          assert.ifError(err);
+          assert(_.isEqual([], res), 'should be empty array');
+          done();
+        });
+      });
+
+    });
+
+    describe('with schema', function() {
+
+      before(function(done) {
+        db.createSchema(schema, function(err) {
+          assert.ifError(err);
+          done();
+        });
+      });
+
+      it('creates a table on the specified schema', function(done) {
+        db.createDocumentTable(schemaTableName, function(err, res) {
+          assert.ifError(err);
+          assert(_.isEqual([], res), 'should be empty array');
+          done();
+        });
+      });
+
+    });
+
+  });
+
+
+  describe('drop', function() {
+
+    describe('without schema', function() {
+
+      before(function(done) {
+        db.createDocumentTable(tableName, function(err) {
+          assert.ifError(err);
+          done();
+        });
+      });
+
+      it('removes the table from public schema', function(done) {
+        db.dropDocumentTable(tableName, {cascade: true}, function(err, res) {
+          assert.ifError(err);
+          assert(_.isEqual([], res), 'should be empty array');
+          assert.equal(undefined, db[tableName]);
+          done();
+        });
+      });
+
+    });
+
+    describe('with schema', function() {
+
+      before(function(done) {
+        db.createSchema(schema, function(err) {
+          assert.ifError(err);
+          db.createDocumentTable(schemaTableName, function(err) {
+            assert.ifError(err);
+            done();
+          });
+        });
+      });
+
+      it('removes the table from the specified schema', function(done) {
+        db.dropDocumentTable(schemaTableName, {cascade: true}, function(err, res) {
+          assert.ifError(err);
+          assert(_.isEqual([], res), 'should be empty array');
+          assert.equal(undefined, db[schema][tableName]);
+          done();
+        });
+      });
+    });
+
+  });
+
+});
--- test/schema_spec.js
+++ test/schema_spec.js
@@ -3,6 +3,77 @@ var helpers = require("./helpers");
 var _ = require("underscore")._;
 var db;
 
+describe('Schema', function() {
+  var schemaName = 'spec';
+  var tableName = 'doggies';
+  var schemaTableName = schemaName + '.' + tableName;
+
+  before(function(done) {
+    helpers.resetDb(function(err,res){
+      db = res;
+      done();
+    });
+  });
+
+  describe('create', function() {
+
+    after(function(done) {
+      db.dropSchema(schemaName, {cascade: true}, function(err) {
+        assert.ifError(err);
+        assert.equal(db[schemaName], undefined);
+        done();
+      });
+    });
+
+    it('adds a new schema', function(done) {
+      db.createSchema(schemaName, function(err) {
+        assert.ifError(err);
+        assert(_.isEqual(db[schemaName], {}), 'should be an empty object');
+        done();
+      });
+    });
+
+  });
+
+  describe('drop', function() {
+
+    beforeEach(function(done) {
+      db.createSchema(schemaName, function(err) {
+        assert.ifError(err);
+        db.createDocumentTable(schemaTableName, function(err) {
+          assert.ifError(err);
+          done();
+        });
+      });
+    });
+
+    after(function(done) {
+      db.dropSchema(schemaName, {cascade: true}, function(err) {
+        assert.ifError(err);
+        done();
+      });
+    });
+
+    it('removes a schema and underlying table with cascade option', function(done) {
+      db.dropSchema(schemaName, {cascade: true}, function(err) {
+        assert.ifError(err);
+        assert.equal(db[schemaName], undefined);
+        done();
+      });
+    });
+
+    it('fails to remove schema and underlying tables without cascade', function(done) {
+      db.dropSchema(schemaName, {cascade: false}, function(err) {
+        assert(err !== null, 'should callback with error');
+        done();
+      });
+    });
+
+  });
+
+});
+
+
 describe('Schema - Bound Tables -Add/Edit/Delete', function () {
 
   //Separate 'suite' for add/edit/delete so query tests don't get borked:
