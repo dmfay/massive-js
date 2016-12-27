e0d90c60a8e136b8b27e9f44b46d0841a76514c7
Dian Fay <dian.m.fay@gmail.com>
2016-12-26 23:55:39 -0500

    first pass on table

--- test/inheritance_spec.js
+++ test/inheritance_spec.js
@@ -8,26 +8,20 @@ describe("Table Inheritance", function () {
   });
 
   describe("Querying", function () {
-    it("includes descendant rows with the parent schema by default", function (done) {
-      db.cities.find(function (err,res) {
-        assert.ifError(err);
+    it("includes descendant rows with the parent schema by default", function () {
+      return db.cities.find().then(res => {
         assert.equal(res.length, 10);
 
         var okc = res.filter(function (c) { return c.name === "Oklahoma City"; })[0];
 
         assert(okc);
         assert.equal(okc.hasOwnProperty("of_state"), false);
-
-        done();
       });
     });
 
-    it("excludes descendant rows if the options specify ONLY", function (done) {
-      db.cities.find({}, {only: true}, function (err,res) {
-        assert.ifError(err);
+    it("excludes descendant rows if the options specify ONLY", function () {
+      return db.cities.find({}, {only: true}).then(res => {
         assert.equal(res.length, 6);
-
-        done();
       });
     });
 
@@ -35,36 +29,26 @@ describe("Table Inheritance", function () {
       assert.equal(db.hasOwnProperty("capitals"), true);
     });
 
-    it("descendant table rows", function (done) {
-      db.capitals.find(function (err,res) {
-        assert.ifError(err);
+    it("descendant table rows", function () {
+      return db.capitals.find().then(res => {
         assert.equal(res.length, 4);
 
         assert.equal(res[0].hasOwnProperty("of_state"), true);
-
-        done();
       });
     });
   });
 
   describe("Writing via the parent table", function () {
-    it("does not update descendant rows from the parent table", function (done) {
-      db.cities.update({name: "Phoenix"}, {population: 1563099}, function (err, res) {
-        assert.ifError(err);
+    it("does not update descendant rows from the parent table", function () {
+      return db.cities.update({name: "Phoenix"}, {population: 1563099}).then(res => {
         assert.equal(res.length, 0);
-
-        done();
       });
     });
 
-    it("does not delete descendant rows from the parent table", function (done) {
-      db.cities.destroy({name: "Nashville"}, function (err, res) {
-        assert.ifError(err);
+    it("does not delete descendant rows from the parent table", function () {
+      return db.cities.destroy({name: "Nashville"}).then(res => {
         assert.equal(res.length, 0);
-
-        done();
       });
     });
-
   });
 });
--- test/table_spec.js
+++ test/table_spec.js
@@ -1,5 +1,7 @@
 const assert = require("chai").assert;
 
+require("co-mocha");
+
 describe('Tables -Add/Edit/Delete', function () {
   var db;
 
@@ -10,19 +12,15 @@ describe('Tables -Add/Edit/Delete', function () {
   describe("Executing inline SQL", function () {
     before(init);
 
-    it('with run and no args returns 4 products', function (done) {
-      db.run("select * from products", function(err,res){
-        assert.ifError(err);
+    it('with run and no args returns 4 products', function () {
+      return db.run("select * from products").then(res => {
         assert.equal(4, res.length);
-        done();
       });
     });
 
-    it('with run and id returns Product 1', function (done) {
-      db.run("select * from products where id=$1",[1], function(err,res){
-        assert.ifError(err);
+    it('with run and id returns Product 1', function () {
+      return db.run("select * from products where id=$1", [1]).then(res => {
         assert.equal(1, res[0].id);
-        done();
       });
     });
   });
@@ -30,22 +28,18 @@ describe('Tables -Add/Edit/Delete', function () {
   describe('save', function () {
     before(init);
 
-    it('adds a product ', function (done) {
-      db.products.save({name : "Gibson Les Paul", description : "Lester's brain child", price : 3500}, function(err, res){
-        assert.ifError(err);
+    it('adds a product ', function () {
+      return db.products.save({name : "Gibson Les Paul", description : "Lester's brain child", price : 3500}).then(res => {
         assert.equal(res.id, 5);
-        done();
       });
     });
 
-    it('updates a product', function (done) {
+    it('updates a product', function () {
       var product = {id : 4, name : "Fender Stratocaster", description : "Leo Fender's baby", price : 1200, tags: ['1', '2']};
-      db.products.save(product, function(err, res){
-        assert.ifError(err);
+      db.products.save(product).then(res => {
         assert.equal(product.id, 4);  // should not clobber the original object
         assert.equal(res.id, 4);
         assert.equal(res.name, "Fender Stratocaster");
-        done();
       });
     });
   });
@@ -53,48 +47,38 @@ describe('Tables -Add/Edit/Delete', function () {
   describe('insert', function () {
     before(init);
 
-    it('inserts a product', function (done) {
-      db.products.insert({name: "A Product"}, function (err, res) {
-        assert.ifError(err);
+    it('inserts a product', function () {
+      return db.products.insert({name: "A Product"}).then(res => {
         assert.equal(res.name, "A Product");
-        done();
       });
     });
 
-    it('inserts multiple products', function (done) {
-      db.products.insert([{name: "A Product"}, {name: "Another Product"}], function (err, res) {
-        assert.ifError(err);
+    it('inserts multiple products', function () {
+      return db.products.insert([{name: "A Product"}, {name: "Another Product"}]).then(res => {
         assert.equal(res.length, 2);
         assert.equal(res[0].name, "A Product");
         assert.equal(res[1].name, "Another Product");
-        done();
       });
     });
 
-    it('inserts nothing', function (done) {
-      db.products.insert([], function (err, res) {
-        assert.ifError(err);
+    it('inserts nothing', function () {
+      return db.products.insert([]).then(res => {
         assert.equal(res.length, 0);
-        done();
       });
     });
 
-    it('inserts array fields', function (done) {
-      db.products.insert({name: "A Product", tags: ['one', 'two']}, function (err, res) {
-        assert.ifError(err);
+    it('inserts array fields', function () {
+      return db.products.insert({name: "A Product", tags: ['one', 'two']}).then(res => {
         assert.equal(res.name, "A Product");
         assert.deepEqual(res.tags, ['one', 'two']);
-        done();
       });
     });
 
     describe('violating a constraint', function () {
-      it('returns a useful error', function(done){
-        db.products.insert({name: null}, function (err) {
-          assert.equal(err.message, 'null value in column "name" violates not-null constraint');
+      it('returns a useful error', function() {
+        return db.products.insert({name: null}).catch(err => {
           assert.equal(err.code, '23502');
           assert.notEqual(err.detail, undefined);
-          done();
         });
       });
     });
@@ -103,80 +87,66 @@ describe('Tables -Add/Edit/Delete', function () {
   describe('update', function () {
     before(init);
 
-    it('updates multiple products', function (done) {
-      db.products.update({in_stock: true}, {in_stock: false}, function(err, res) {
-        assert.ifError(err);
+    it('updates multiple products', function () {
+      return db.products.update({in_stock: true}, {in_stock: false}).then(res => {
         assert.equal(res.length, 2);
         assert.equal(res[0].id, 1);
         assert.equal(res[0].in_stock, false);
         assert.equal(res[1].id, 2);
         assert.equal(res[1].in_stock, false);
-        done();
       });
     });
 
-    it('updates all products', function (done) {
-      db.products.update({}, {price: 1.23}, function(err, res) {
-        assert.ifError(err);
+    it('updates all products', function () {
+      return db.products.update({}, {price: 1.23}).then(res => {
         assert.equal(res.length, 4);
         assert.equal(res[0].price, 1.23);
         assert.equal(res[1].price, 1.23);
         assert.equal(res[2].price, 1.23);
         assert.equal(res[3].price, 1.23);
-        done();
       });
     });
 
-    it('updates products with predicates of varying length', function (done) {
-      db.products.update({'specs !=': null}, {price: 1.23}, function(err, res) {
-        assert.ifError(err);
+    it('updates products with predicates of varying length', function () {
+      return db.products.update({'specs !=': null}, {price: 1.23}).then(res => {
         assert.equal(res.length, 3);
         assert.equal(res[0].price, 1.23);
         assert.equal(res[1].price, 1.23);
         assert.equal(res[2].price, 1.23);
-        done();
       });
     });
 
-    it('updates multiple products with an IN list', function (done) {
-      db.products.update({id: [1, 2]}, {price: 123.45}, function(err, res) {
-        assert.ifError(err);
+    it('updates multiple products with an IN list', function () {
+      return db.products.update({id: [1, 2]}, {price: 123.45}).then(res => {
         assert.equal(res.length, 2);
         assert.equal(res[0].id, 1);
         assert.equal(res[0].price, 123.45);
         assert.equal(res[1].id, 2);
         assert.equal(res[1].price, 123.45);
-        done();
       });
     });
 
-    it('updates multiple products with a NOT IN list', function (done) {
-      db.products.update({'id !=': [1, 2]}, {price: 543.21}, function(err, res) {
-        assert.ifError(err);
+    it('updates multiple products with a NOT IN list', function () {
+      return db.products.update({'id !=': [1, 2]}, {price: 543.21}).then(res => {
         assert.equal(res.length, 2);
         assert.equal(res[0].id, 3);
         assert.equal(res[0].price, 543.21);
         assert.equal(res[1].id, 4);
         assert.equal(res[1].price, 543.21);
-        done();
       });
     });
 
-    it('returns a product when there are no fields to be updated', function (done) {
-      db.products.update({id: 1}, function(err, res) {
-        assert.ifError(err);
+    it('returns a product when there are no fields to be updated', function () {
+      return db.products.update({id: 1}).then(res => {
         assert.equal(res.id, 1);
         assert.equal(res.name, 'Product 1');
-        done();
       });
     });
 
-    it('returns multiple products when there are no fields to be updated', function (done) {
-      db.products.update({id: [1, 2]}, {}, function(err, res) {
-        assert.ifError(err);
+    it('returns multiple products when there are no fields to be updated', function () {
+      return db.products.update({id: [1, 2]}, {}).then(res => {
         assert.equal(res[0].id, 1);
         assert.equal(res[0].name, 'Product 1');
-        done();
       });
     });
   });
@@ -184,143 +154,106 @@ describe('Tables -Add/Edit/Delete', function () {
   describe('destroy', function () {
     before(init);
 
-    it('deletes a product ', function (done) {
-      db.products.destroy({id : 4}, function(err, deleted){
-        db.products.find(4, function(err, found) {
-          assert.ifError(err);
-          //Deleted returns an array...
-          assert(found === undefined && deleted[0].id === 4);
-          done();
-        });
-      });
+    it('deletes a product', function* () {
+      const deleted = yield db.products.destroy({id : 4});
+      assert.lengthOf(deleted, 1);
+      assert.equal(deleted[0].id, 4);
+
+      const found = yield db.products.find(4);
+      assert.isUndefined(found);
     });
 
-    it('deletes all products', function (done) {
-      db.products.destroy({}, function(err, deleted){
-        db.products.find({}, function(err, found) {
-          assert.ifError(err);
-          assert.equal(deleted.length, 3);
-          assert.equal(found.length, 0);
-          done();
-        });
-      });
+    it('deletes all products', function* () {
+      const deleted = yield db.products.destroy({});
+      assert.equal(deleted.length, 3);
+
+      const found = yield db.products.find({});
+      assert.equal(found.length, 0);
     });
 
-    it('deletes by matching json', function (done) {
-      db.docs.destroy({'body->>title': 'A Document'}, function(err, deleted) {
-        assert.ifError(err);
-        assert.equal(deleted.length, 1);
+    it('deletes by matching json', function* () {
+      const deleted = yield db.docs.destroy({'body->>title': 'A Document'});
+      assert.equal(deleted.length, 1);
 
-        db.docs.find({id: deleted[0].id}, function (err, found) {
-          assert.ifError(err);
-          assert.equal(found.length, 0);
-          done();
-        });
-      });
+      const found = yield db.docs.find({id: deleted[0].id});
+      assert.equal(found.length, 0);
     });
 
-    it('deletes by matching json with whitespace', function (done) {
-      db.docs.destroy({'body ->> title': 'Another Document'}, function(err, deleted) {
-        assert.ifError(err);
-        assert.equal(deleted.length, 1);
+    it('deletes by matching json with whitespace', function* () {
+      const deleted = yield db.docs.destroy({'body ->> title': 'Another Document'});
+      assert.equal(deleted.length, 1);
 
-        db.docs.find({id: deleted[0].id}, function (err, found) {
-          assert.ifError(err);
-          assert.equal(found.length, 0);
-          done();
-        });
-      });
+      const found = yield db.docs.find({id: deleted[0].id});
+      assert.equal(found.length, 0);
     });
 
-    it('deletes by matching json with quotes', function (done) {
-      db.docs.destroy({'"body" ->> \'title\'': 'Starsky and Hutch'}, function(err, deleted) {
-        assert.equal(deleted.length, 1);
+    it('deletes by matching json with quotes', function* () {
+      const deleted = yield db.docs.destroy({'"body" ->> \'title\'': 'Starsky and Hutch'});
+      assert.equal(deleted.length, 1);
 
-        db.docs.find({id: deleted[0].id}, function (err, found) {
-          assert.ifError(err);
-          assert.equal(found.length, 0);
-          done();
-        });
-      });
+      const found = yield db.docs.find({id: deleted[0].id});
+      assert.equal(found.length, 0);
     });
   });
 
   describe('Add/Update/Delete records with nonstandard casing:', function() {
     before(init);
 
-    it('adds a User ', function (done) {
-      db.Users.save({Email : "foo@bar.com", Name: "Another test user"}, function(err, res){
-        assert.ifError(err);
+    it('adds a User', function () {
+      return db.Users.save({Email : "foo@bar.com", Name: "Another test user"}).then(res => {
         assert.equal(res.Id, 2);
         assert.equal(res.Email, "foo@bar.com");
-        done();
       });
     });
-    it('updates a User ', function (done) {
-      db.Users.save({Id : 2, Email : "bar@foo.com"}, function(err, res){
-        assert.ifError(err);
+
+    it('updates a User', function () {
+      return db.Users.save({Id : 2, Email : "bar@foo.com"}).then(res => {
         assert.equal(res.Id, 2);
         assert.equal(res.Email, "bar@foo.com");
-        done();
       });
     });
-    it('deletes a User ', function (done) {
-      db.Users.destroy({Id : 2}, function(err, deleted){
-        assert.ifError(err);
-        db.Users.find(2, function(err, found) {
-          assert.ifError(err);
-          //Deleted returns an array...
-          assert(found === undefined && deleted[0].Id == 2);
-          done();
-        });
-      });
+
+    it('deletes a User', function* () {
+      const deleted = yield db.Users.destroy({Id : 2});
+      assert.lengthOf(deleted, 1);
+      assert.equal(deleted[0].Id, 2);
+
+      const found = yield db.Users.find(2);
+      assert.isUndefined(found);
     });
   });
 
   describe('Add/Update/Delete records with UUID keys:', function() {
     before(init);
 
-    it('adds an order', function (done) {
-      db.orders.save({product_id: 1, user_id: 1, notes: 'hi'}, function(err, res) {
-        assert.ifError(err);
+    it('adds an order', function () {
+      return db.orders.save({product_id: 1, user_id: 1, notes: 'hi'}).then(res => {
         assert.ok(res.id !== null);
         assert.equal(res.product_id, 1);
         assert.equal(res.user_id, 1);
         assert.equal(res.notes, 'hi');
-        done();
       });
     });
 
-    it('updates an order', function (done) {
-      db.orders.findOne({}, function(err, found) {
-        assert.ifError(err);
+    it('updates an order', function* () {
+      const found = yield db.orders.findOne({});
+      found.notes = 'hello';
 
-        found.notes = 'hello';
-
-        db.orders.save(found, function(err, res) {
-          assert.ifError(err);
-          assert.equal(res.id, found.id);
-          assert.equal(res.notes, 'hello');
-          done();
-        });
-      });
+      const res = yield db.orders.save(found);
+      assert.equal(res.id, found.id);
+      assert.equal(res.notes, 'hello');
     });
 
-    it('deletes an order', function (done) {
-      db.orders.findOne({}, function(err, found) {
-        assert.ifError(err);
+    it('deletes an order', function* () {
+      const found = yield db.orders.findOne({});
+      assert.isOk(found);
 
-        db.orders.destroy({id : found.id}, function(err, deleted) {
-          assert.ifError(err);
+      const deleted = yield db.orders.destroy({id : found.id});
+      assert.lengthOf(deleted, 1);
+      assert.equal(deleted[0].id, found.id);
 
-          db.orders.findOne({id : found.id}, function(err, remaining) {
-            assert.ifError(err);
-            assert.equal(deleted[0].id, found.id);
-            assert.ok(remaining === undefined);
-            done();
-          });
-        });
-      });
+      const remaining = yield db.orders.findOne({id : found.id});
+      assert.lengthOf(remaining, 0);
     });
   });
 });
