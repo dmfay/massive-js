9c85bf08d0d433db6193de53e9a4f5a3e3ac1a79
Dian Fay <dian.m.fay@gmail.com>
2016-12-27 20:37:39 -0500

    last of the doc table test reorg

--- test/document_table_spec.js
+++ test/document_table_spec.js
@@ -16,11 +16,13 @@ describe('Document queries', function () {
       assert.isFunction(db.docs.findDoc);
       assert.isFunction(db.docs.saveDoc);
       assert.isFunction(db.docs.searchDoc);
+      assert.isFunction(db.docs.setAttribute);
 
       assert.instanceOf(db.myschema.docs, Table);
       assert.isFunction(db.myschema.docs.findDoc);
       assert.isFunction(db.myschema.docs.saveDoc);
       assert.isFunction(db.myschema.docs.searchDoc);
+      assert.isFunction(db.myschema.docs.setAttribute);
     });
   });
 
@@ -259,6 +261,54 @@ describe('Document queries', function () {
         assert.equal(docs[0].title, 'Another Document');
         assert.equal(docs[1].title, 'A Document');
       });
+    });
+  });
+
+  describe("setAttribute", function() {
+    var newDoc = {};
+    var array = [1,2,3];
+
+    before(function() {
+      return db.saveDoc("docs", {name:"Foo", score:1}).then(doc => newDoc = doc);
+    });
+
+    it('check saved attribute', function() {
+      assert.equal(1, newDoc.score);
+    });
+
+    it('updates the document', function() {
+      return db.docs.setAttribute(newDoc.id, "vaccinated", true).then(doc => {
+        assert.equal(doc.vaccinated, true);
+      });
+    });
+
+    it('updates the document when passed array value', function() {
+      return db.docs.setAttribute(newDoc.id, "array", array).then(doc => {
+        assert.deepEqual(doc.array, array);
+      });
+    });
+
+    it('updates the document without replacing existing attributes', function() {
+      return db.docs.setAttribute(newDoc.id, "score", 99).then(doc => {
+        assert.equal(doc.score, 99);
+        assert.equal(doc.vaccinated, true);
+        assert.equal(doc.id, newDoc.id);
+        assert.deepEqual(doc.array, array);
+      });
+    });
+
+    it('escapes values properly', function() {
+      return db.docs.setAttribute(newDoc.id, "field", "value").then(doc => {
+        assert.equal(doc.score, 99);
+        assert.equal(doc.vaccinated, true);
+        assert.equal(doc.field, 'value');
+        assert.equal(doc.id, newDoc.id);
+        assert.deepEqual(doc.array, array);
+      });
+    });
+
+    after(function () {
+      return db.docs.destroy({id: newDoc.id});
     });
   });
 });
