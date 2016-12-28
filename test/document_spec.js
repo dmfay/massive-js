439f1efff7859135253d9a173c93c04ac6a148df
Dian Fay <dian.m.fay@gmail.com>
2016-12-27 20:14:51 -0500

    more test consolidation

--- test/document_spec.js
+++ test/document_spec.js
@@ -115,7 +115,57 @@ describe('Index document functions', function () {
       });
     });
 
-    describe('without existing table', function () {
+    describe("with an existing table in a schema", function () {
+      before(function* () {
+        yield db.createSchema("myschema");
+
+        return db.createDocumentTable("myschema.docs");
+      });
+
+      it("saves a new document without an id", function () {
+        return db.saveDoc("myschema.docs", {
+          title: "Alone",
+          description: "yearning in the darkness",
+          price: 89.99,
+          is_good: true,
+          created_at: "2015-03-04T09:43:41.643Z"
+        }).then(doc => {
+          assert.equal(doc.title, "Alone");
+          assert.equal(doc.id, 1);
+        });
+      });
+
+      it("updates an existing document with an id", function* () {
+        const doc = yield db.saveDoc("myschema.docs", {
+          title: "Alone",
+          description: "yearning in the darkness",
+          price: 89.99,
+          is_good: true,
+          created_at: "2015-03-04T09:43:41.643Z"
+        });
+
+        assert.isOk(db.myschema);
+        assert.equal(doc.title, "Alone");
+        assert.isOk(doc.id);
+
+        doc.title = "Together!";
+
+        const updated = yield db.saveDoc("myschema.docs", doc);
+
+        assert.equal(updated.id, doc.id);
+        assert.equal(updated.title, "Together!");
+
+        const retrieved = yield db.myschema.docs.findDoc(doc.id);
+
+        assert.equal(retrieved.title, "Together!");
+      });
+
+      after(function() {
+        return db.dropSchema("myschema", {cascade: true});
+      });
+    });
+
+    describe('without an existing table', function () {
       it('creates a table if none exists', function* () {
         const doc = yield db.saveDoc("doggies", {name: "Fido", age: 10});
 
@@ -125,6 +175,23 @@ describe('Index document functions', function () {
 
       after(function() {
         return db.query("DROP TABLE doggies;");
+      });
+    });
+
+    describe('without an existing table in a schema', function () {
+      before(function () {
+        return db.createSchema("myschema");
+      });
+
+      it('creates a table if none exists', function* () {
+        const doc = yield db.saveDoc("myschema.doggies", {name: "Fido", age: 10});
+
+        assert.isOk(db.doggies);
+        assert.equal(doc.name, "Fido");
+      });
+
+      after(function() {
+        return db.dropSchema("myschema", {cascade: true});
       });
     });
   });
