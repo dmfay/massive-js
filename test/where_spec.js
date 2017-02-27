1fa0909f0bfee0246578b2510060da2a64d3de4d
Dian Fay <dian.m.fay@gmail.com>
2017-02-26 23:51:53 -0500

    refactoring query building

--- test/where_spec.js
+++ test/where_spec.js
@@ -391,31 +391,39 @@ describe('WHERE clause generation', function () {
 
   describe('isPkSearch', function () {
     it('should pass integers', function () {
-      assert.equal(where.isPkSearch(1), true);
+      assert.equal(where.isPkSearch(1, {}), true);
     });
 
     it('should pass v1 uuids', function () {
-      assert.equal(where.isPkSearch('a432c1f6-0443-11e6-b512-3e1d05defe78'), true);
+      assert.equal(where.isPkSearch('a432c1f6-0443-11e6-b512-3e1d05defe78', {}), true);
     });
 
     it('should pass v4 uuids', function () {
-      assert.equal(where.isPkSearch('a2a072cc-7a41-4fd6-bd16-ae1677166c05'), true);
+      assert.equal(where.isPkSearch('a2a072cc-7a41-4fd6-bd16-ae1677166c05', {}), true);
     });
 
     it('should reject integers in strings', function () {
-      assert.equal(where.isPkSearch('1'), false);
+      assert.equal(where.isPkSearch('1', {}), false);
     });
 
     it('should reject badly versioned uuids', function () {
-      assert.equal(where.isPkSearch('a2a072cc-7a41-9fd6-bd16-ae1677166c05'), false);
+      assert.equal(where.isPkSearch('a2a072cc-7a41-9fd6-bd16-ae1677166c05', {}), false);
     });
 
     it('should reject badly formatted uuids', function () {
-      assert.equal(where.isPkSearch('a2a072cc7a419fd6bd16ae1677166c05'), false);
+      assert.equal(where.isPkSearch('a2a072cc7a419fd6bd16ae1677166c05', {}), false);
     });
 
-    it('should reject objects', function () {
-      assert.equal(where.isPkSearch({id: 1}), false);
+    it('should accept simple document criteria', function () {
+      assert.equal(where.isPkSearch({id: 1}, {document: true}), true);
+    });
+
+    it('should accept complex document criteria', function () {
+      assert.equal(where.isPkSearch({'id >=': 1}, {document: true}), true);
+    });
+
+    it('should reject objects if not looking at a document', function () {
+      assert.equal(where.isPkSearch({id: 1}, {}), false);
     });
   });
 });
