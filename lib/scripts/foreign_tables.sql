e8b466cb3913a31a763c6337cd493d7bba4d4c8d
Dian Fay <dian.m.fay@gmail.com>
2017-04-18 15:52:31 -0400

    fix query & filter tests

--- lib/scripts/foreign_tables.sql
+++ lib/scripts/foreign_tables.sql
@@ -3,7 +3,7 @@
 select table_schema as schema, table_name as name, is_insertable_into
 from information_schema.tables
 where table_type = 'FOREIGN TABLE'
-  and ((
+  and (((
       -- allow specific schemas (none or '' assumes all):
       case when $1 ='' then 1=1
       else table_schema = any(string_to_array(replace($1, ' ', ''), ','))
@@ -19,6 +19,6 @@ where table_type = 'FOREIGN TABLE'
       -- Below can use '%' as wildcard. Change 'like' to '=' to require exact names:
     else replace((table_schema || '.'|| table_name), 'public.', '') like any(string_to_array(replace($3, ' ', ''), ','))
     end
-  )
+  ))
 order by table_schema, table_name;
 
