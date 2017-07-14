5605a5c0092997137e1ea11417fb1876c740f978
Dian Fay <dian.m.fay@gmail.com>
2017-07-14 19:18:06 -0400

    support transaction tags

--- test/transactions.js
+++ test/transactions.js
@@ -20,11 +20,41 @@ describe('transactions', function () {
     });
   });
 
+  it('does things in a tagged transaction', function () {
+    return db.transaction('mytag', () => {
+      return Promise.all([
+        db.t1.insert({id: 1}),
+        db.t2.insert({id: 1}),
+      ]);
+    }).then(() => {
+      return db.t1.count({}).then(count => {
+        assert.equal(count, 1);
+      });
+    });
+  });
+
   it('does things in a transaction and includes a custom mode', function () {
     const TransactionMode = db.pgp.txMode.TransactionMode;
     const isolationLevel = db.pgp.txMode.isolationLevel;
 
     return db.transaction(() => {
+      return Promise.all([
+        db.t1.insert({id: 1}),
+        db.t2.insert({id: 1}),
+      ]);
+    }, new TransactionMode({tiLevel: isolationLevel.serializable})).then(() => {
+      return db.t1.count({}).then(count => {
+        assert.equal(count, 1);
+      });
+    });
+  });
+
+
+  it('does things in a tagged transaction and includes a custom mode', function () {
+    const TransactionMode = db.pgp.txMode.TransactionMode;
+    const isolationLevel = db.pgp.txMode.isolationLevel;
+
+    return db.transaction('mytag', () => {
       return Promise.all([
         db.t1.insert({id: 1}),
         db.t2.insert({id: 1}),
