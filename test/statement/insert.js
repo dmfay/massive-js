acbef84e3bb5b25c4785de367386741f4c820cb4
Dian Fay <dian.m.fay@gmail.com>
2017-09-14 18:05:28 -0400

    less fragile array insert (fixes #465)

--- test/statement/insert.js
+++ test/statement/insert.js
@@ -27,21 +27,31 @@ describe('Insert', function () {
     it('should return a basic update statement for the specified changes', function () {
       const result = new Insert(source, {field1: 'value1'});
       assert.equal(result.format(), 'INSERT INTO testsource ("field1") VALUES ($1) RETURNING *');
+      assert.deepEqual(result.params, ['value1']);
     });
 
     it('should join fields and values with commas', function () {
       const result = new Insert(source, {field1: 'value1', field2: 2});
       assert.equal(result.format(), 'INSERT INTO testsource ("field1", "field2") VALUES ($1, $2) RETURNING *');
+      assert.deepEqual(result.params, ['value1', 2]);
     });
 
     it('should handle multiple records', function () {
       const result = new Insert(source, [{field1: 'value1', field2: 2}, {field1: 'value2', field2: 3}]);
       assert.equal(result.format(), 'INSERT INTO testsource ("field1", "field2") VALUES ($1, $2), ($3, $4) RETURNING *');
+      assert.deepEqual(result.params, ['value1', 2, 'value2', 3]);
+    });
+
+    it('should handle multiple records with fields out of order', function () {
+      const result = new Insert(source, [{field1: 'value1', field2: 2}, {field2: 3, field1: 'value2'}]);
+      assert.equal(result.format(), 'INSERT INTO testsource ("field1", "field2") VALUES ($1, $2), ($3, $4) RETURNING *');
+      assert.deepEqual(result.params, ['value1', 2, 'value2', 3]);
     });
 
     it('should handle onConflictIgnore option', function () {
       const result = new Insert(source, {field1: 'value1'}, {onConflictIgnore: true});
       assert.equal(result.format(), 'INSERT INTO testsource ("field1") VALUES ($1) ON CONFLICT DO NOTHING RETURNING *');
+      assert.deepEqual(result.params, ['value1']);
     });
   });
 });
