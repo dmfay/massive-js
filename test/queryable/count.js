95ca49ec11fa1f0d45a50f272a1f8e0ab73bcc6e
robinaugh <jrobinaugh@gmail.com>
2017-09-30 13:09:16 -0400

    countjs uses singletable schema (#486)
    * createDb uses empty schema
    
    * countjs uses singletable schema

--- test/queryable/count.js
+++ test/queryable/count.js
@@ -4,7 +4,7 @@ describe('count', function () {
   let db;
 
   before(function () {
-    return resetDb().then(instance => db = instance);
+    return resetDb('singletable').then(instance => db = instance);
   });
 
   after(function () {
