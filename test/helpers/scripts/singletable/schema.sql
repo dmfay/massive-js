93f3a0092a45fd2806ab15dba3156d3434da7285
Jason Robinaugh <jrobinaugh@gmail.com>
2017-09-30 13:47:15 -0400

    insertjs uses singletable scheme

--- test/helpers/scripts/singletable/schema.sql
+++ test/helpers/scripts/singletable/schema.sql
@@ -4,7 +4,13 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 CREATE TABLE products (
         id serial PRIMARY KEY,
-        string varchar NOT NULL
+        string varchar NOT NULL,
+        tags character varying(255)[],
+        description text,
+        specs jsonb,
+        price decimal(10,2) default 0.00,
+        uuid uuid default gen_random_uuid(),
+        "CaseName" varchar
 );
 
 INSERT INTO products (string) VALUES ('one');
