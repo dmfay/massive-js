5a38966975bfa38bbb60e0ebce5700ab9a02033a
Tarig <alcook16@gmail.com>
2017-09-30 13:21:31 -0400

    have finddoc test use data-docs DB

--- test/helpers/scripts/data-docs/schema.sql
+++ test/helpers/scripts/data-docs/schema.sql
@@ -6,6 +6,16 @@ create table docs(
   search tsvector
 );
 
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+create table uuid_docs(
+  id uuid primary key default gen_random_uuid(),
+  body jsonb not null,
+  search tsvector
+);
+
+
+insert into uuid_docs(body) values ('{"things": "stuff"}');
 
 insert into docs(body)
 values('{"title":"Document 1","price":22,"description":"lorem ipsum etc","is_good":true,"created_at":"2015-03-04T09:43:41.643Z"}'),
