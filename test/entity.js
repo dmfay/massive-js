75de5717c2ee37fee7b2e16411bc23effca1bcb3
Dian Fay <dian.m.fay@gmail.com>
2018-01-11 17:32:46 -0500

    refactor: centralize path and name assembly

--- test/entity.js
+++ test/entity.js
@@ -3,17 +3,35 @@
 const Entity = require('../lib/entity');
 
 describe('Entity', function () {
-  it('should create a base entity from a spec', function () {
+  it('should use the spec path if there is one', function () {
     const e = new Entity({
       db: 'connected instance',
-      path: 'one.two',
+      path: 'one.test_entity',
       name: 'test_entity',
       schema: 'one'
     });
 
     assert.deepEqual(e, {
       db: 'connected instance',
-      path: 'one.two',
+      path: 'one.test_entity',
+      name: 'test_entity',
+      schema: 'one',
+      delimitedName: '"test_entity"',
+      delimitedSchema: '"one"',
+      delimitedFullName: '"one"."test_entity"'
+    });
+  });
+
+  it('should create a base entity from a spec', function () {
+    const e = new Entity({
+      db: 'connected instance',
+      name: 'test_entity',
+      schema: 'one'
+    });
+
+    assert.deepEqual(e, {
+      db: 'connected instance',
+      path: 'one.test_entity',
       name: 'test_entity',
       schema: 'one',
       delimitedName: '"test_entity"',
@@ -25,18 +43,18 @@ describe('Entity', function () {
   it('should default to the current schema', function () {
     const e = new Entity({
       db: {currentSchema: 'other'},
-      path: 'one.two',
-      name: 'test_entity'
+      name: 'test_entity',
+      schema: 'other'
     });
 
     assert.deepEqual(e, {
       db: {currentSchema: 'other'},
-      path: 'one.two',
+      path: 'test_entity',
       name: 'test_entity',
       schema: 'other',
       delimitedName: '"test_entity"',
       delimitedSchema: '"other"',
-      delimitedFullName: '"other"."test_entity"'
+      delimitedFullName: '"test_entity"'
     });
   });
 });
