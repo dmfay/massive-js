e56efd9b4051dd40c678bbeeef5c62c649cd8341
Dian Fay <dmfay@users.noreply.github.com>
2018-02-08 17:54:14 -0500

    docs: comments on document tables (#535)

--- lib/scripts/document-table.sql
+++ lib/scripts/document-table.sql
@@ -6,3 +6,9 @@ CREATE TABLE ${schema~}.${table~}(
 );
 CREATE INDEX idx_${index^} ON ${schema~}.${table~} USING GIN(body jsonb_path_ops);
 CREATE INDEX idx_search_${index^} ON ${schema~}.${table~} USING GIN(search);
+
+COMMENT ON TABLE ${schema~}.${table~} IS 'A document table generated with Massive.js.';
+COMMENT ON COLUMN ${schema~}.${table~}.id IS 'The document primary key. Will be added to the body when retrieved using Massive document functions';
+COMMENT ON COLUMN ${schema~}.${table~}.body IS 'The document body, stored without primary key.';
+COMMENT ON COLUMN ${schema~}.${table~}.search IS 'Search vector for full-text search support.';
+COMMENT ON COLUMN ${schema~}.${table~}.created_at IS 'Timestamp for document creation.';
