3b8737568fbf2168cd2e446c601b7880b38ba046
Dian Fay <dian.m.fay@gmail.com>
2018-03-31 17:51:37 -0400

    chore: update more tests, reject properly

--- test/table/update.js
+++ test/table/update.js
@@ -4,7 +4,7 @@ describe('update', function () {
   let db;
 
   before(function () {
-    return resetDb().then(instance => db = instance);
+    return resetDb('updatables').then(instance => db = instance);
   });
 
   after(function () {
@@ -13,114 +13,112 @@ describe('update', function () {
 
   describe('single-object updates', function () {
     it('returns null when updating a nonexistent record', function () {
-      return db.products.update({id: 99999, name: 'new and improved product 4'}).then(res => {
+      return db.normal_pk.update({id: 99999, field1: 'something'}).then(res => {
         assert.isNull(res);
       });
     });
 
     it('throws when updating a record with operations in the pk field', function () {
-      assert.throws(() => {
-        db.products.update({'id <': 123, name: 'use other form for bulk updates'});
-      });
+      return db.normal_pk.update({
+        'id <': 123,
+        field1: 'something'
+      })
+        .then(() => { assert.fail(); })
+        .catch(err => {
+          assert.isOk(err);
+        });
     });
 
     it('updates a product by the primary key of a single record', function () {
-      return db.products.update({id: 4, name: 'new and improved product 4'}).then(res => {
-        assert.equal(res.id, 4);
-        assert.equal(res.name, 'new and improved product 4');
+      return db.normal_pk.update({id: 2, field1: 'something'}).then(res => {
+        assert.equal(res.id, 2);
+        assert.equal(res.field1, 'something');
       });
     });
 
     it('returns a product when there are no fields to be updated', function () {
-      return db.products.update({id: 1}).then(res => {
+      return db.normal_pk.update({id: 1}).then(res => {
         assert.equal(res.id, 1);
-        assert.equal(res.name, 'Product 1');
+        assert.equal(res.field1, 'alpha');
+      });
+    });
+
+    it('updates an array field', function () {
+      return db.normal_pk.update({id: 1, array_field: ['one', 'two']}).then(res => {
+        assert.equal(res.id, 1);
+        assert.lengthOf(res.array_field, 2);
+        assert.deepEqual(res.array_field, ['one', 'two']);
       });
     });
 
     it('updates an empty array field', function () {
-      return db.products.update({id: 1, tags: []}).then(res => {
+      return db.normal_pk.update({id: 1, array_field: []}).then(res => {
         assert.equal(res.id, 1);
-        assert.lengthOf(res.tags, 0);
+        assert.lengthOf(res.array_field, 0);
       });
     });
 
     it('updates a record in a table with a Cased Name', function () {
-      return db.Users.update({Id: 1, Email: 'bar@foo.com'}).then(res => {
+      return db.CasedName.update({Id: 1, Field1: 'Omega'}).then(res => {
         assert.equal(res.Id, 1);
-        assert.equal(res.Email, 'bar@foo.com');
+        assert.equal(res.Field1, 'Omega');
       });
     });
 
     it('applies options', function () {
-      return db.products.update({id: 1, name: 'another kind of product'}, null, {build: true}).then(res => {
+      return db.normal_pk.update({id: 1, field1: 'omega'}, null, {build: true}).then(res => {
         assert.deepEqual(res, {
-          sql: 'UPDATE "products" SET "name" = $1 WHERE "id" = $2 RETURNING *',
-          params: ['another kind of product', 1]
+          sql: 'UPDATE "normal_pk" SET "field1" = $1 WHERE "id" = $2 RETURNING *',
+          params: ['omega', 1]
         });
       });
     });
   });
 
   describe('bulk updates', function () {
-    it('updates multiple products', function () {
-      return db.products.update({in_stock: true}, {in_stock: false}).then(res => {
-        assert.equal(res.length, 2);
-        assert.notEqual(res[0].id, res[1].id);
-        assert.isFalse(res[0].in_stock);
-        assert.isFalse(res[1].in_stock);
+    it('updates multiple normal_pk', function () {
+      return db.normal_pk.update({'id >': 2}, {field1: 'zeta'}).then(res => {
+        assert.equal(res.length, 1);
+        assert.equal(res[0].id, 3);
+        assert.equal(res[0].field1, 'zeta');
       });
     });
 
-    it('updates all products', function () {
-      return db.products.update({}, {price: 1.23}).then(res => {
-        assert.equal(res.length, 4);
-        assert.equal(res[0].price, 1.23);
-        assert.equal(res[1].price, 1.23);
-        assert.equal(res[2].price, 1.23);
-        assert.equal(res[3].price, 1.23);
-      });
-    });
-
-    it('updates products with predicates of varying length', function () {
-      return db.products.update({'specs !=': null}, {price: 1.23}).then(res => {
+    it('updates all normal_pk', function () {
+      return db.normal_pk.update({}, {field1: 'upsilon'}).then(res => {
         assert.equal(res.length, 3);
-        assert.equal(res[0].price, 1.23);
-        assert.equal(res[1].price, 1.23);
-        assert.equal(res[2].price, 1.23);
+        assert.isTrue(res.every(r => r.field1 === 'upsilon'));
       });
     });
 
-    it('updates multiple products with an IN list', function () {
-      return db.products.update({id: [1, 2]}, {price: 123.45}).then(res => {
+    it('updates multiple normal_pk with an IN list', function () {
+      return db.normal_pk.update({id: [1, 2]}, {field1: 'theta'}).then(res => {
         assert.equal(res.length, 2);
         assert.notEqual(res[0].id, res[1].id);
-        assert.equal(res[0].price, 123.45);
-        assert.equal(res[1].price, 123.45);
+        assert.isTrue(res.every(r => r.field1 === 'theta'));
       });
     });
 
-    it('updates multiple products with a NOT IN list', function () {
-      return db.products.update({'id !=': [1, 2]}, {price: 543.21}).then(res => {
-        assert.equal(res.length, 2);
-        assert.notEqual(res[0].id, res[1].id);
-        assert.equal(res[0].price, 543.21);
-        assert.equal(res[1].price, 543.21);
+    it('updates multiple normal_pk with a NOT IN list', function () {
+      return db.normal_pk.update({'id !=': [1, 2]}, {field1: 'tau'}).then(res => {
+        assert.equal(res.length, 1);
+        assert.equal(res[0].id, 3);
+        assert.equal(res[0].field1, 'tau');
       });
     });
 
-    it('returns multiple products when there are no fields to be updated', function () {
-      return db.products.update({id: [1, 2]}, {}).then(res => {
-        assert.equal(res[0].id, 1);
-        assert.equal(res[0].name, 'Product 1');
+    it('returns multiple normal_pk when there are no fields to be updated', function () {
+      return db.normal_pk.update({id: [1, 2]}, {}).then(res => {
+        assert.isTrue(res.some(r => r.id === 1));
+        assert.isTrue(res.some(r => r.id === 2));
       });
     });
 
     it('applies options', function () {
-      return db.products.update({id: [1, 2]}, {name: 'another kind of product'}, {build: true}).then(res => {
+      return db.normal_pk.update({id: [1, 2]}, {field1: 'iota'}, {build: true}).then(res => {
         assert.deepEqual(res, {
-          sql: 'UPDATE "products" SET "name" = $1 WHERE "id" IN ($2,$3) RETURNING *',
-          params: ['another kind of product', 1, 2]
+          sql: 'UPDATE "normal_pk" SET "field1" = $1 WHERE "id" IN ($2,$3) RETURNING *',
+          params: ['iota', 1, 2]
         });
       });
     });
