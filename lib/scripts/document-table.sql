25212d93ee580d2fc42ba90c334b128e5128c273
Dian Fay <dian.m.fay@gmail.com>
2018-05-13 23:31:04 -0400

    feat: metadata in documents BREAKING CHANGE: 'created_at' and 'updated_at' are now reserved keys in documents

--- lib/scripts/document-table.sql
+++ lib/scripts/document-table.sql
@@ -2,13 +2,31 @@ CREATE TABLE ${schema~}.${table~}(
   id serial PRIMARY KEY,
   body jsonb NOT NULL,
   search tsvector,
-  created_at timestamptz DEFAULT now()
+  created_at timestamptz DEFAULT now(),
+  updated_at timestamptz
 );
+
 CREATE INDEX idx_${index^} ON ${schema~}.${table~} USING GIN(body jsonb_path_ops);
 CREATE INDEX idx_search_${index^} ON ${schema~}.${table~} USING GIN(search);
+
+CREATE OR REPLACE FUNCTION massive_document_updated()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+SECURITY definer
+AS $$
+BEGIN
+  NEW.updated_at = now();
+  RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER ${schema^}_${table^}_updated
+BEFORE UPDATE ON ${schema~}.${table~}
+FOR EACH ROW EXECUTE PROCEDURE massive_document_updated();
 
 COMMENT ON TABLE ${schema~}.${table~} IS 'A document table generated with Massive.js.';
 COMMENT ON COLUMN ${schema~}.${table~}.id IS 'The document primary key. Will be added to the body when retrieved using Massive document functions';
 COMMENT ON COLUMN ${schema~}.${table~}.body IS 'The document body, stored without primary key.';
 COMMENT ON COLUMN ${schema~}.${table~}.search IS 'Search vector for full-text search support.';
 COMMENT ON COLUMN ${schema~}.${table~}.created_at IS 'Timestamp for document creation.';
+COMMENT ON COLUMN ${schema~}.${table~}.created_at IS 'Timestamp for the record''s last modification.';
